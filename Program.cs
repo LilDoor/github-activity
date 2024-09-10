72ba148b4b566e9452352befd9b3cc72255f0268
Nurasil Meirbek <mieiirbiek2004@gmail.com>
2024-09-10 20:13:37 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace github_activity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
